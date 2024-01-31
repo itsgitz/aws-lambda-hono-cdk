9d44c117a44876ec22fe12fb491cf5f4adb2205e
itsgitz <anggit.ginanjar.dev@gmail.com>
2024-01-31 19:09:39 +0700

    chore: initial commits

---
+++ test/aws-lambda-hono-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsLambdaHonoCdk from '../lib/aws-lambda-hono-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-lambda-hono-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsLambdaHonoCdk.AwsLambdaHonoCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
